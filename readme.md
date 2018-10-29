c426960c8c212a06d1e7640e9e8deb68a88f5bdf
Aldrich Allen Barcenas <aldrich.barcenas@gmail.com>
2018-10-29 21:32:08 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+#3 Sandbox Webpack Configuration
+
+this is the webpack configuration for the `react-sandbox-v2` but it can also be used as webpack config for other projects aswell.
+
+## Setup
+
+You need to configure your `.env` file as it pulls the details for the folder structure of your project
+
+* **SRC_FOLDER** - Normally either its `app` or `src`
+* **WEBPACK_BUILD_FOLDER** - the folder name of the folder where the build files to be placed. It is either `static` or `build` and it is placed on the root directory of the project
+* **WEBPACK_ENTRY_PATH** - the path of the file where the webpack will base its build. Normally it is `app.js` or `index.js`
+
---
+++ webpack.config.js
@@ -0,0 +1,103 @@
+require('dotenv').config()
+
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const autoPrefixer = require('autoprefixer')
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+module.exports = (env, options) => {
+  const isDevMode = options.mode === 'development'
+  const srcFolder = process.env.SCR_FOLDER
+
+  const config = {
+    entry: process.env.WEBPACK_ENTRY_PATH,
+    target: 'web',
+    output: {
+      path: path.join(__dirname, '..', process.env.WEBPACK_BUILD_FOLDER),
+      filename: '[name].bundle.js',
+      chunkFilename: 'bundle.[chunkhash].js'
+    },
+    devtool: (isDevMode) ? 'inline-source-map' : false,
+    resolve: {
+      alias: {
+        'Components': path.resolve(srcFolder, 'components'),
+        'Containers': path.resolve(srcFolder, 'containers')
+      }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(srcFolder, 'index.html')
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css'
+      })
+    ],
+    devServer: {
+      publicPath: '/',
+      port: 3100,
+      historyApiFallback: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            {loader: isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader},
+            {loader: 'css-loader'},
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: () => [autoPrefixer]
+              }
+            },
+            {loader: 'sass-loader'}
+          ]
+        },
+        {
+          test: /\.(ttf|eot|woff|woff2)$/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: 'fonts/[name].[ext]'
+            }
+          }
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg|ico)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'assets/'
+              }
+            }
+          ]
+        }
+      ]
+    },
+    node: {
+      fs: 'empty',
+      child_process: 'empty'
+    }
+  }
+
+  /*
+  uncomment this if you want to analyze the build sizes of your build file
+  if (!isDevMode) {
+    config.plugins.push(
+      new BundleAnalyzerPlugin()
+    )
+  }
+  */
+
+  return config
+}
